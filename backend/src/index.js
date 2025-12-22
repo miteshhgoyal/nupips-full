a55ee1cdcf7f7ba8f8c3a647fa5286cd4db5ad50
Mitesh Goyal <miteshgoyal.00@gmail.com>
2025-12-22 18:51:57 +0530

    server fixes again

--- backend/src/index.js
+++ backend/src/index.js
@@ -48,7 +48,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions));
 
-app.use(express.json({ limit: '10mb' }));
+app.use(express.json({ limit: '500mb' }));
 app.use(express.urlencoded({ extended: true }));
 
 // Routes
